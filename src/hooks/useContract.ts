a636aeae3313910a61b774f8bdd0f97b1c2e3244
deva panda <66827166+devapanda@users.noreply.github.com>
2020-09-13 20:32:40 +0530

    updated

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,6 +1,6 @@
 import { Contract } from '@ethersproject/contracts'
 import { ChainId, WETH } from '@uniswap/sdk'
-import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
+import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/ISwapdexV2Pair.json'
 import { useMemo } from 'react'
 import ENS_ABI from '../constants/abis/ens-registrar.json'
 import ENS_PUBLIC_RESOLVER_ABI from '../constants/abis/ens-public-resolver.json'
